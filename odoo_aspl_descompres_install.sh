2a824a18c4b62ed781c74cf5c2542e5f89a6a777
Tito026_Thinkpad <tito.guerrero026@gmail.com>
2019-10-19 15:43:08 -0500

    Absolute Ref to home

--- odoo_aspl_descompres_install.sh
+++ odoo_aspl_descompres_install.sh
@@ -1,2 +1,2 @@
 mv /mnt/extra-addons/odoo_pos_reports/ /mnt/extra-addons/aspl_pos_report
-cd home && tar -xzvf aspl_pos_report.tar.gz -C /mnt/extra-addons && chown -R odoo:odoo /mnt/extra-addons/aspl_pos_report && chmod -R 755 /mnt/extra-addons/aspl_pos_report/ && exit
+cd /home && tar -xzvf aspl_pos_report.tar.gz -C /mnt/extra-addons && chown -R odoo:odoo /mnt/extra-addons/aspl_pos_report && chmod -R 755 /mnt/extra-addons/aspl_pos_report/ && exit
--- odoo_aspl_download_install.sh
+++ odoo_aspl_download_install.sh
@@ -1,1 +1,1 @@
-cd home && curl -O -J http://192.168.0.11/aspl_pos_report.tar.gz && exit
+cd /home && curl -O -J http://192.168.0.11/aspl_pos_report.tar.gz && exit
